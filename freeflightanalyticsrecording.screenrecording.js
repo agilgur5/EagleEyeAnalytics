7b07c77c37fce9d853d22c98a42a4a1793a5f5a2
Anton Gilgur <agilgur5@gmail.com>
2014-09-27 22:37:47 -0400

    switch framerate to delay

--- freeflightanalyticsrecording.screenrecording.js
+++ freeflightanalyticsrecording.screenrecording.js
@@ -5,9 +5,9 @@ FreeFlightAnalyticsRecording.ScreenRecordingModule = {};
 
 /** start recording by saving a list of pngs
  *  @param duration the number of milliseconds to record for
- *  @param framerate the framerate at which to record at
+ *  @param delay the delay after every screenshot (inverse of framerate)
  */ 
-FreeFlightAnalyticsRecording.ScreenRecordingModule.startRecording = function(duration, framerate) {
+FreeFlightAnalyticsRecording.ScreenRecordingModule.startRecording = function(duration, delay) {
   var screenshotList = []; // list of screenshot pngs to send to backend to create gifs from
   // take screenshots until duration has been reached
   // push a screenshot to the screenshot list according to the framerate
@@ -16,7 +16,7 @@ FreeFlightAnalyticsRecording.ScreenRecordingModule.startRecording = function(dur
       var image = canvas.toDataURL("image/png");
       screenshotList.push(image);
     });
-  }, 1000 / framerate);
+  }, delay);
   
   setTimeout(function() {
     clearInterval(intervalId);
@@ -24,7 +24,7 @@ FreeFlightAnalyticsRecording.ScreenRecordingModule.startRecording = function(dur
     $.ajax({
       type: 'POST',
       url: FreeFlightAnalyticsRecording.RouteMap.screenRecordingURL,
-      data: {framerate: framerate, screenshotList: screenshotList}
+      data: {delay: delay, screenshotList: screenshotList}
     });
   }, duration);
 }
